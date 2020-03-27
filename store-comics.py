8d2dd20aa64ccd8f39dd3b431ad2a6c0826fd7fc
naman ranawat <naman159@namans-MacBook-Pro.local>
2020-03-27 18:47:46 +0530

    Ready for me

--- store-comics.py
+++ store-comics.py
@@ -1,27 +1,45 @@
 input1 = 0
+print("Welcome to your very own Comic DataBase. Your listings are saved in \"comics-list.txt\"! help command can be used to check the features.")
 
 def main():
     f = open("comics-list.txt", "a+")
     global input1
     input1 = input()
-    det = input1[0:3]
-    comic1 = input1.split(None, 1)[1]
 
-    if(det == "add"):
-        delete()
-        add()
-        main()
+    if(input1 == "bye"):
+        print("Have a good Day!")
+    elif(input1 == "help"):
+        print("add: Add a comic or increase the number of chaps read for a certain comic to the list.")
+        print("fnd: Find the comic to check the number of chps you have read.")
+        print("del: Delete a comic listing.")
+        print("lst: List all the comics in your list.")
+        print("Example: add abc-1")
+    else:
+        det = input1[0:3]
 
-    if(det == "fnd"):
-        b = find()
-        if(b == False):
-            print("The comic could not be found in the db!")
-        main()
+        if(det == "add"):
+            delete()
+            add()
+            main()
 
-    if(det == "del"):
-        l = delete()
-        print(comic1 +" removed!")
-        main()
+        elif(det == "fnd"):
+            b = find()
+            if(b == False):
+                print("The comic could not be found in the db!")
+            main()
+
+        elif(det == "del"):
+            l = delete()
+            print(comic1 +" removed!")
+            main()
+
+        elif(det == "lst"):
+            list()
+            main()
+
+        else:
+            print("Invalid Command!")
+            main()
 
 def add():
     f = open("comics-list.txt", "a+")
@@ -38,7 +56,7 @@ def find():
     for i in comics:
         a = i.split("-", 1)[0]
         if(a == comic1):
-            print(i)
+            print(istrip('\n'))
             return True
     return False
 
@@ -53,5 +71,11 @@ def delete():
             if(a != comic1):
                 f.write(i)
     return comic1
+
+def list():
+    with open("comics-list.txt", "r") as f:
+        comics = f.readlines()
+    for i in comics:
+        print(i.strip('\n'))
 
 main()
